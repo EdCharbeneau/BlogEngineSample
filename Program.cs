6903a791c778ea87284ad9c8016a45f5603b4889
Ed Charbeneau <ed.charbeneau@gmail.com>
2022-06-30 10:54:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using RazorLight;
+using System.Reflection;
+
+
+var engine = new RazorLightEngineBuilder()
+    .UseFileSystemProject(
+        Path.GetDirectoryName(
+        Assembly.GetExecutingAssembly().Location)
+        )
+    .UseMemoryCachingProvider()
+    .Build();
+
+ViewModel model = new();
+string result = await engine.CompileRenderAsync("index.cshtml", model);
+
+Console.WriteLine(result);
+Console.ReadKey();
+
+public class ViewModel
+{
+    public string Title { get; set; } = "Hello World";
+
+    public string Body { get; set; } = "Greatest Blogpost ever";
+}
+
